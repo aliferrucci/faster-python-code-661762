055d8c7fa4712da338ae2dab585033e191f4b7cf
Alexandra Ferrucci <36905791+aliferrucci@users.noreply.github.com>
2025-06-22 02:42:25 +0000

    Notes on memry_profiler

--- Ch01/01_07/sos.py
+++ Ch01/01_07/sos.py
@@ -1,6 +1,15 @@
 """memory_profiler example"""
 
 
+# Add the memory_profiler decorator
+# Terminal: python -m memory_profiler sos.py
+# Shows memory usage on line 9
+# If use mprof:
+# remove decorator and change line 22 to 100,000,000
+# and run: mprof run sos.py in terminal and view file
+# with mprof plot name_of_file.dat
+# show memory usage over time
+@profile
 def sum_of_diffs(vals):
     """Compute sum of diffs"""
     vals2 = vals[1:]
